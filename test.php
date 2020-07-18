c84d48b57d86b19082fa76be5b8b01b9b5310730
fukushima hayao <nu0640042@gmail.con>
2020-07-19 01:10:00 +0900

    書き加え。

--- test.php
+++ test.php
@@ -7,7 +7,9 @@ use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverExpectedCondition;
  
-$word = 'test';
+$word = 'amazon';
+
+$title = "$word - Buy $word at Best Price in Philippines | www.lazada.com.ph";
 
 $options = new ChromeOptions();
 $options->addArguments(['--headless']);
@@ -21,12 +23,23 @@ $element = $driver->findElement(WebDriverBy::name('q'));
 $element->sendKeys($word);
 $element->submit();
 $driver->wait(15)->until(
-    WebDriverExpectedCondition::titleIs("$word - Buy test at Best Price in Philippines | www.lazada.com.ph")
+    WebDriverExpectedCondition::titleIs($title)
 );
 
-if ($driver->getTitle() !== "$word - Buy test at Best Price in Philippines | www.lazada.com.ph") {
+if ($driver->getTitle() !== "$title") {
     throw new Exception('fail');
 }
-$file = "サンプル_chrome.png";
-$driver->takeScreenshot($file);
+
+// $file = "サンプル_chrome.png";
+// $driver->takeScreenshot($file);
+
+$testId = $driver->findElement(WebDriverBy::className('c16H9d'));
+$text = $testId->getText();
+echo $text;
+
+// $a = $driver->findElemenet(WebDriverBy::className("lzd-logo-content"));
+// foreach ($a as $v) {
+//     echo  $v; 
+// }
+
 $driver->close();
